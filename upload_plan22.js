f5111e4c389eb9c9325e7792198ec708e17bda87
Alexandra Popescu <lixapopescu@gmail.com>
2015-05-22 23:10:10 +0200

    plan22. almost there

--- upload_plan22.js
+++ upload_plan22.js
@@ -3,7 +3,7 @@ load("reteta_dorada_la_cuptor_salata.js");
 load("reteta_crap_salata_fasole_verde.js");
 load("reteta_tocanita_pui_ghimbir.js");
 load("reteta_hamburger_cod_macrou_salata.js");
-load("reteta_pui_kiev_ciuperci_marinate.js");
+//load("reteta_pui_kiev_ciuperci_marinate.js");
 
 load("plan22.js");
 load("check_collections_length.js");
